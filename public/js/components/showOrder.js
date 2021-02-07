e98655f72d19e6aebf90b8a23bef1296a31a8832
Tim <nowaybuticanfly@gmail.com>
2021-02-07 17:06:48 +0300

    UI рефактор

---
+++ public/js/components/showOrder.js
@@ -0,0 +1,31 @@
+window.onload = function () {
+    //select  status
+
+    let select = document.getElementById('status');
+    select.onchange = function () {
+        console.log('here');
+        fetch(window.location.href + '?status=' + select.value, {
+            headers: {
+                'X-CSRF-TOKEN': document.getElementById('csrf-token').content,
+            },
+            method: 'PATCH',
+        });
+    }
+
+
+
+    //delete btn
+
+    let deleteBtn = document.querySelector('.order-main-card_btn');
+
+    deleteBtn.onclick = function ()  {
+        return fetch(window.location.href, {
+            headers: {
+                'X-CSRF-TOKEN': document.getElementById('csrf-token').content,
+            },
+            method: 'delete'
+        })
+            .then(() => window.location.href =  '/orders');
+    }
+
+}
